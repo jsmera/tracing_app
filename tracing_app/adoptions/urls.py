7384a717f3e2aedc12d868391e8151c36a02c6ab
Josue Pena <jozdashh@javerianacali.edu.co>
2020-10-27 16:59:16 -0500

    adopters

--- tracing_app/adoptions/urls.py
+++ tracing_app/adoptions/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import PetsListView, PetCreateView, PetUpdateView
+from .views import PetsListView, PetCreateView, PetUpdateView, AdopterListView, AdopterCreateView, AdopterUpdateView
 
 app_name = "adoptions"
 urlpatterns = [
     path("pets/", PetsListView.as_view(), name='pet-list'),
     path("pets/new", PetCreateView.as_view(), name="pet-new"),
     path("pets/<int:pk>", PetUpdateView.as_view(), name="pet-edit"),
+    path("adopters/", AdopterListView.as_view(), name='adopter-list'),
+    path("adopters/new", AdopterCreateView.as_view(), name='adopter-new'),
+    path("adopters/<int:pk>", AdopterUpdateView.as_view(), name="adopter-edit"),
 ]
--- tracing_app/adoptions/views.py
+++ tracing_app/adoptions/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse 
-from .models import Pet
-from .forms import CreatePetForm
-
+from .models import Pet, Adopter
+from .forms import CreatePetForm, CreateAdopterForm
 
 class PetsListView(ListView):
     model = Pet
@@ -24,8 +23,24 @@ class PetCreateView(CreateView):
 class PetUpdateView(UpdateView):
     model = Pet
     form_class = CreatePetForm
-    
+
     def get_success_url(self):
         return reverse("adoptions:pet-list")
 
+class AdopterListView(ListView):
+    model = Adopter
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
+class AdopterCreateView(CreateView):
+    model = Adopter
+    form_class = CreateAdopterForm
+    def get_success_url(self):
+        return reverse("adoptions:adopter-list")
+
+class AdopterUpdateView(UpdateView):
+    model = Adopter
+    form_class = CreateAdopterForm
+    def get_success_url(self):
+        return reverse("adoptions:adopter-list")
